6540a02eafb0e6836200e699d8a202753226c888
Roman Yudin <yudiol114@gmail.com>
2023-10-25 00:18:39 +0300

    feat: создать User

---
+++ src/main/java/com/yudiol/jobsearchplatform/model/User.java
@@ -0,0 +1,33 @@
+package com.yudiol.jobsearchplatform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
